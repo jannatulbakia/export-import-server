81cb1e513a16e6086bea96364e308a71ac2d46ca
bakia <bakia.nimmy@gmail.com>
2025-11-15 21:17:19 +0600

    name changed

--- seed.js
+++ seed.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const seedDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
